96108f1332eb9343e5f0509c0da28c5d60f06780
Meera Verma <meeraverma0712@gmail.com>
2026-01-26 14:52:03 +0530

    initial js setup

---
+++ first.js
@@ -0,0 +1,19 @@
+
+const Product = {
+    name:"Ball Pen",
+    rating:4,
+    offer: 5,
+    price : 250,
+};
+ console.log(Product);
+
+
+
+const profile ={
+    name: "Meera Verma",
+    posts: 195,
+    followers:145,
+    following :20,
+    profession: "Developer",
+    description:"to educate someone is the highest privilege",
+}; 
