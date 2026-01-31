3d193181ee69a79aa6d7827f6396f876afad8952
Meera Verma <meeraverma0712@gmail.com>
2026-01-31 22:29:38 +0530

    new practice qs

---
+++ arrays/practice1.js
@@ -0,0 +1,23 @@
+// let items =[250,645,300,900,50];
+
+// for(let i=0;i<items.length;i++){
+//   let offer=items[i]/10;
+//     items[i]-= offer;
+// }
+
+// console.log(`items after discount ${items}`);
+
+
+
+// let fruits =["apple", "banana", "watermelon", "strawberry"];
+// fruits.push("mango");
+// console.log(fruits);
+
+
+
+
+let companies= ["bloomberg","microsoft","uber","google","IBM","netflix" ]
+companies.shift(0);
+companies.splice(1,1,"ola");
+companies.push("amazon");
+console.log(companies);
