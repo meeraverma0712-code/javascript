d81af9106bbcadf4821581c92f1d46656301b6d3
Meera Verma <meeraverma0712@gmail.com>
2026-02-03 22:58:56 +0530

    initialised new qs error check

--- DOM/script.js
+++ DOM/script.js
@@ -1,22 +1,22 @@
-// let heading = document.getElementById("heading");
-// console.dir(heading);
+let heading = document.getElementById("heading");
+console.dir(heading);
 
-// let heading = document.getElementsByClassName("heading");
-// console.dir(heading);
+let heading = document.getElementsByClassName("heading");
+console.dir(heading);
 
-//  let paras = document.getElementsByTagName("p");
-//  console.dir(paras);
-//  console.log(paras);
+ let paras = document.getElementsByTagName("p");
+ console.dir(paras);
+ console.log(paras);
 
 // QUERY SELECTOR
 
 //first element
-// let elements = document. querySelector("p");
-// console.dir(elements);
+let elements = document. querySelector("p");
+console.dir(elements);
 
 //all elementlist
-// let allElements = document. querySelectorAll("p");
-// console.dir(allElements);
+let allElements = document. querySelectorAll("p");
+console.dir(allElements);
 
 let div= document.querySelector("div");
 console.dir(div);
