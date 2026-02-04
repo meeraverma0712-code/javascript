8e9747d770a6b2e2981cddcf341ce7685426757b
Meera Verma <meeraverma0712@gmail.com>
2026-02-04 22:01:49 +0530

    initialised dom practice question

---
+++ DOM/part2.js
@@ -0,0 +1,11 @@
+let newBtn = document.createElement("button");
+newBtn.innerText ="click mee";
+
+newBtn.style.color ="white";
+newBtn.style.backgroundColor = "red";
+document.querySelector("body").prepend(newBtn);
+
+
+
+
+let para = document.querySelector("p");
